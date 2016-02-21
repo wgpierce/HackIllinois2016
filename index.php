567d48ee383af53cc482a28a010d93706f7267ce
William <scrupulous128@gmail.com>
2016-02-21 02:52:46 -0500

    Added python
    Added python, cleaned up

--- index.php
+++ index.php
@@ -4,22 +4,12 @@
 ?>
       <!-- Main jumbotron for a primary marketing message or call to action -->
       <div class="jumbotron">
-		<p>Click buttons below to track who has been using the farm equipment and for how long</p>
+		<p style="font-size: 3em">Click buttons below to track who has been using the farm equipment and for how long</p>
       </div>
 
-
       <div class="page-header">
-        <h1>Buttons</h1>
         <a href="track_machine.php"><button type="button" class="btn btn-lg btn-primary">Track A Machine</button></a>
-        <button type="button" class="btn btn-lg btn-primary">Track A User</button>
-
-<!--
-        <button type="button" class="btn btn-lg btn-success">Success</button>
-        <button type="button" class="btn btn-lg btn-info">Info</button>
-        <button type="button" class="btn btn-lg btn-warning">Warning</button>
-        <button type="button" class="btn btn-lg btn-danger">Danger</button>
-        <button type="button" class="btn btn-lg btn-link">Link</button>
---> 
+        <a href="track_people.php"><button type="button" class="btn btn-lg btn-success">Track A User</button></a>
       </div>
 
 <?php include('include/footer.html'); ?>
--- track_machine.php
+++ track_machine.php
@@ -2,33 +2,20 @@
 	include('/include/head.html');
 	include('/include/header.html');
 ?>
-		<div id="id01"></div>
-<script>
-var MyJohnDeere = require('myjohndeere');
 
+<?php 
 
-
-var xmlhttp = new XMLHttpRequest();
-var url = "myTutorials.txt";
-
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var myArr = JSON.parse(xmlhttp.responseText);
-        myFunction(myArr);
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-function myFunction(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' + 
-        arr[i].display + '</a><br>';
-    }
-    document.getElementById("id01").innerHTML = out;
+if (isset($_POST['submit'])) {
+	$userName = $_POST['userName'];
+	echo "You entered $userName<br>";	
+		
+} else {
+	echo '<form action="track_machine.php" method="POST" enctype="multipart/form-data">
+			<label>Name of User:<input type="text" name="userName"></label><br>
+			<input type="submit" value="Submit" name="submit">
+		</form>';
 }
-</script>
+
+?>
 
 <?php include('include/footer.html'); ?>
